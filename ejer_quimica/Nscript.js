e136ec8a6fad641b8f8e2a6b970568a93dbb5aa6
jesushurtado20 <jesus.hurtado.20@monzon.salesianos.org>
2025-10-31 09:48:08 +0100

    Add files via upload

---
+++ ejer_quimica/Nscript.js
@@ -0,0 +1,22 @@
+//Oxigeno funcion
+
+function Sodio() {
+	document.getElementById("Nombre").textContent = "Sodio";
+	document.getElementById("Sigla").textContent = "Na";
+	document.getElementById("Peso").textContent = "22.99";
+	document.getElementById("Número").textContent = "11";
+}
+
+function Oxígeno() {
+	document.getElementById("Nombre").textContent = "Oxígeno";
+	document.getElementById("Sigla").textContent = "O";
+	document.getElementById("Peso").textContent = "16";
+	document.getElementById("Número").textContent = "8";
+}
+
+function Carbono() {
+	document.getElementById("Nombre").textContent = "Carbono";
+	document.getElementById("Sigla").textContent = "C";
+	document.getElementById("Peso").textContent = "12.011";
+	document.getElementById("Número").textContent = "6";
+}
